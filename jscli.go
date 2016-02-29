9e214123c69d500eabe0ef0195dc1752421f8bdf
Kuo-tung Kao <ya790206@gmail.com>
2016-02-29 21:26:40 +0800

    allow jscli to call external program

--- jscli.go
+++ jscli.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/robertkrimen/otto"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -12,7 +15,30 @@ var (
 	codePtr      = kingpin.Arg("code", "code").Required().String()
 	lineSeqPtr   = kingpin.Flag("line-seq", "code").Short('l').Default("\n").String()
 	columnSeqPtr = kingpin.Flag("column-seq", "code").Short('c').Default(" ").String()
+	funcListPtr  = ArgsStrList(kingpin.Flag("funcion", "function").Short('f'))
+	pathListPtr  = ArgsStrList(kingpin.Flag("path", "command search path").Short('p'))
 )
+
+type argsStrList []string
+
+func (i *argsStrList) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+func (i *argsStrList) String() string {
+	return ""
+}
+
+func (i *argsStrList) IsCumulative() bool {
+	return true
+}
+
+func ArgsStrList(s kingpin.Settings) (target *[]string) {
+	target = new([]string)
+	s.SetValue((*argsStrList)(target))
+	return
+}
 
 func readAll() string {
 	bytes, err := ioutil.ReadAll(os.Stdin)
@@ -31,12 +57,60 @@ func setPrintArrayFunc(vm *otto.Otto) {
 		for(i=0;i<arr.length;i+=1){
 			console.log(i)
 		}
+	}`)
+
+}
+
+func callExternalFunc(cmd string, args []string) string {
+	out, err := exec.Command(cmd, args...).Output()
+	if err != nil {
+		panic(err)
 	}
-`)
+	return string(out)
+
+}
+
+func addPATHEnv(path string) {
+	oldEnv := os.Getenv("PATH")
+	newEnv := path + ":" + oldEnv
+	os.Setenv("PATH", newEnv)
+}
+
+func getWd() string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return cwd
+}
+
+func initExternelFunc(vm *otto.Otto, cmdPath string) {
+	funcName := cmdPath[:len(cmdPath)-len(filepath.Ext(cmdPath))]
+	vm.Set(funcName, func(call otto.FunctionCall) otto.Value {
+		args := make([]string, len(call.ArgumentList))
+		for i := 0; i < len(call.ArgumentList); i += 1 {
+			ret, err := call.Argument(i).ToString()
+			if err == nil {
+				args[i] = ret
+			} else {
+				panic(err)
+			}
+
+		}
+		output := callExternalFunc(cmdPath, args)
+		result, err := vm.ToValue(output)
+		if err != nil {
+			panic(err)
+		}
+
+		return result
+	})
 
 }
 
 func main() {
+	addPATHEnv(getWd())
+	fmt.Printf("")
 	kingpin.Parse()
 
 	stdin := readAll()
@@ -55,6 +129,14 @@ func main() {
 	vm.Set("matrix", *matrixPtr)
 	vm.Run("print = console.log")
 	setPrintArrayFunc(vm)
+
+	for i := 0; i < len(*funcListPtr); i += 1 {
+		initExternelFunc(vm, (*funcListPtr)[i])
+	}
+
+	for i := 0; i < len(*pathListPtr); i += 1 {
+		addPATHEnv((*pathListPtr)[i])
+	}
 
 	vm.Run(*codePtr)
 }
